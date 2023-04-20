b1b24d7fb142767b762f45bfd5f195ed5cc1ca66
makrofaj67 <114802232+makrofaj67@users.noreply.github.com>
2023-04-20 20:49:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,4 +33,14 @@ print("Mean Squared Error:", mse)
 rmse = mean_squared_error(y_test, y_pred, squared=False)
 print("Root Mean Squared Error:", rmse)
 
+new_home = {'Num_Of_Room': 3, 'Area': 120, 'Age': 10, 'Apt_Floor': 2, 
+            'Credit_Eli': 1, 'Items': 0, 'Num_of_Bath': 2, 'Floor_basement': 0,
+            'Floor_first floor': 0, 'Floor_interstage': 1, 'Floor_top floor': 0,
+            'Heat_type_Kombi': 1, 'Heat_type_Merkezi': 0, 'Heat_type_Merkezi (Pay Ölçer)': 0,
+            'Usage_Boş': 0, 'Usage_Kiracılı': 1, 'Usage_Mülk Sahibi': 0}
+
+new_home_df = pandas.DataFrame([new_home])
+predicted_price = reg.predict(new_home_df)[0]
+
+print('Predicted price for the new home: tl', predicted_price)
 
